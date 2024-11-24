30a4da5079bf68aca390af128b446d20236d597e
Vadym16 <117903952+Vadym16@users.noreply.github.com>
2024-11-25 00:55:12 +0100

    Add project files.

---
+++ BB№2.1/BB№2.1.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Transport {
+public:
+
+    Transport() {
+        type = "Транспортний засіб";
+    }
+      
+    void show() {
+        cout << "Тип: " << type << endl;
+    }
+
+protected:
+    string type;
+};
+
+class Car : public Transport {
+public:
+    Car() {
+        type = "Автомобіль";
+    }
+
+    void show() {
+        cout << "Тип: " << type << endl;
+    }
+
+};
+
+class Train : public Transport {
+public:
+    Train() {
+        type = "Потяг";
+    }
+
+    void show() {
+        cout << "Тип: " << type << endl;
+    }
+
+};
+
+class Express : public Train {
+public:
+    Express() {
+        type = "експрес Потяг";
+    }
+
+    void show() {
+        cout << "Тип: " << type << endl;
+    }
+};
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "ukr");
+
+    Transport tr;
+    Car car;
+    Train train;
+    Express expr;
+
+    tr.show();
+    car.show();
+    train.show();
+    expr.show();
+}
+
